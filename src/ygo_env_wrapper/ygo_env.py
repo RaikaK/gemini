cbc15ee0cb4168e280e1cc8d7487fac50833a7b9
inusan_star <haruki.room.14814@icloud.com>
2025-09-08 09:09:10 +0900

    update env

--- src/ygo_env_wrapper/ygo_env.py
+++ src/ygo_env_wrapper/ygo_env.py
@@ -3,10 +3,11 @@ import sys
 sys.path.append("C:/Users/b1/Desktop/master-duel-ai")
 
 import argparse
-import random
-import time
+from pprint import pprint
 
 from ygo.udi_io import UdiIO
+from ygo.util.text import TextUtil
+from ygo.models import DuelEndData
 from ygo.models.duel_state_data import DuelStateData
 from ygo.models.command_request import CommandRequest, CommandEntry
 from ygo import constants
@@ -17,72 +18,107 @@ from src.ygo_env_wrapper.action_data import ActionData
 
 WAITING_TIME = 1
 
+
 class YgoEnv:
-    def __init__(self, udi_io: UdiIO, reward_func: BaseRewardFunction):
+    def __init__(self, udi_io: UdiIO, text_util: TextUtil, reward_func: BaseRewardFunction):
         self.udi_io = udi_io  # MDクライアントの情報参照
+        self.text_util = text_util
+        if self.text_util is None:
+            self.text_util = TextUtil()
 
         self.reward_func = reward_func  # 報酬関数
         # 何も指定がない場合は、 NormalRewardFunctionを使用
         if self.reward_func is None:
             self.reward_func = NormalRewardFunction(udi_io, is_normalized=True)
-
-        
-        self.is_cmd_required = False
-        # self.udi_io.wait(lambda _: self._mainloop)
+    
+    def reset(self):
+        return self.step(None)
 
     def step(
         self, action_data: ActionData, 
-    ) -> dict[str, DuelStateData | CommandRequest | float | bool]:
+    ) -> dict:
         """
         コマンド(cmd_index)を実行し、
         is_cmd_required==Trueのときreturnする
-        - 次状態: "next_state"
+        - デュエルスタートかどうか： "is_duel_start"
+        - デュエルエンドかどうか: "is_duel_end"
+        - アクション選択が必要かどうか: "is_cmd_required"
+        - デュエル終了時の結果データ: "duel_end_result"
+        - 状態: "state"
         - コマンドリクエスト: "command_request"
         - 報酬: "reward"
-        - 終了フラグ: "done"
         を返す
         """
-        
-        while not self.udi_io.input():
-            print("wating udi_io.input() ")
-            time.sleep(WAITING_TIME)
-
+        # コマンドの実行
+        reward: float = 0.0
         if action_data is not None:
+            # print("send cmd")
             cmd_index = action_data.command_index
             # cmd_indexの実行
             self.udi_io.output_command(cmd_index)
-        
-        
-        # ##############
-        # コマンド実行後
-        # ##############
-        # コマンド要求まで待機
-        while not self.udi_io.is_command_required():
-            time.sleep(WAITING_TIME)
-            print("waiting ...")
-        
-        
-        next_state: DuelStateData = self.udi_io.get_duel_state_data()  # 次状態
-        command_request = (
-            self.udi_io.get_command_request()
-        )  # 選択可能なコマンドリストなどの情報
+            # 報酬を計算
+            reward = self.reward_func.eval(action_data=action_data)
+    
+        try:
+            if (not self.udi_io.input()):
+                pass
+            elif (self.udi_io.duel_data != {}):
+                state: DuelStateData = self.udi_io.get_duel_state_data()  # 次状態
+                command_request: CommandRequest = self.udi_io.get_command_request()  # 選択可能なコマンドリストなどの情報
+                
 
-        # 報酬を計算
-        reward: float = self.reward_func.eval(action_data=action_data)
-
-        done = self.udi_io.is_duel_end()  # 終了フラグ
-
-
+                
+        except Exception as e:
+            print(e)
+            sys.exit()
+        
+    
+        # set each flags
+        is_duel_start: bool = self.udi_io.is_duel_start()
+        is_duel_end: bool = self.udi_io.is_duel_end()
+        is_cmd_required: bool = self.udi_io.is_command_required()
+        duel_end_data: DuelEndData = self.udi_io.get_duel_end_data() if is_duel_end else None
+        
         result_dict = {
-            "next_state": next_state,
+            "is_duel_start": is_duel_start,
+            "is_duel_end": is_duel_end,
+            "is_cmd_required": is_cmd_required,
+            "duel_end_data": duel_end_data,
+            "state": state,
             "command_request": command_request,
             "reward": reward,
-            "done": done,
         }
+        # print(f"is_cmd_required: {self.udi_io.is_command_required()}, current_phase: {state.general_data.current_phase}")
         
         return result_dict
 
 
+def cli_player(result:dict) -> ActionData:
+    """CLIからコマンドインデックスう入力できるテスト用のプレイヤー"""
+    # コマンドCLI入力
+    can_send = False
+    cmd_index = -1
+
+    while not can_send:
+        print(f"select your action: [0 - {len(result["command_request"].commands)-1}]")
+        for i, cmd_entry in enumerate(result["command_request"].commands):
+            print(f"command {i}: {text_util.get_command_entry_text(cmd_entry)}")
+
+        try:
+            cmd_index = int(input())
+            if 0 <= cmd_index < len(result["command_request"].commands):
+                can_send = True
+
+        except Exception as e:
+            can_send = False
+            print(f"Invalid command index. Select cmd_index in 0-{len(result["command_request"].commands)-1}")
+    
+    # コマンド生成
+    state = result["state"]
+    cmd_request = result["command_request"]
+    cmd_entry = cmd_request.commands[cmd_index]
+    action_data = ActionData(state=state, command_request=cmd_request, command_entry=cmd_entry)
+    return action_data
 
 if __name__ == "__main__":
     print("起動コマンド（通常）")
@@ -114,17 +150,26 @@ if __name__ == "__main__":
     udi_io = UdiIO(tcpport=args.tcpport, tcp_host=args.tcphost, connect=connect, api_version=1)
     #UDIのログは大量に出るため、出力しないようにする
     udi_io.log_response_history = False
+    text_util = TextUtil()
 
-    env = YgoEnv(udi_io=udi_io, reward_func=None)
+    env = YgoEnv(udi_io=udi_io, text_util=text_util, reward_func=None)
     print("finish initialized")
-    
-    initial_result = env.step(None)
-    print("finish first step()")
 
-    done = initial_result["done"]
-    while not done:
-        cmd = input(f"select your aciton: [0 - {initial_result["command_request"].commands}]")
-        result = env.step(cmd)
-        breakpoint()
+    action_data = None
+    result = env.reset() # 初期化
 
-        done = result["done"]
+    while True:
+        # デュエルの開始 or 終了
+        if result["is_duel_start"]:
+            print("Duel Start")
+        elif result["is_duel_end"]:
+            print("Duel End")
+            print(result["duel_end_data"])
+        
+        action_data = None
+        if result["is_cmd_required"]:
+            
+            # 行動選択
+            action_data = cli_player(result=result)
+        
+        result = env.step(action_data=action_data)
