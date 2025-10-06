03439454270effcc7cc3415bf287efe9cd5f6ff5
inusan-star <haruki.room.14814@icloud.com>
2025-10-06 11:16:28 +0900

    src以外をリファクタ

--- vendor/UDI/libs/ygo/build/lib/ygo/image/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Copyright (C) 2025 Konami Digital Entertainment Co., Ltd. All rights reserved.
-"""GUIで使用する画像に関するパッケージ"""
-
-from .image_manager import ImageManager
--- vendor/UDI/libs/ygo/build/lib/ygo/util/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Copyright (C) 2025 Konami Digital Entertainment Co., Ltd. All rights reserved.
-"""AIエージェント実装時に使う補助的なモジュールのパッケージ"""
-
-from .text import TextUtil
-from .card import CardUtil
-from .udi_log import UdiLogUtil
