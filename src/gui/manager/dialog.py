0f0488720893c56e892c6eca539233d4f337b206
inusan-star <haruki.room.14814@icloud.com>
2025-10-21 22:34:51 +0900

    修正

--- src/gui/manager/dialog.py
+++ src/gui/manager/dialog.py
@@ -14,7 +14,7 @@ class GUIDialog(DialogManager):
         super().__init__(udi_gui_frame, master, **key)
 
         factor = self.udi_gui_frame.factor
-        scaled_font_size = int(Const.DIALOG_FONT[1] * factor)
+        scaled_font_size = int(int(Const.DIALOG_FONT[1]) * factor)
         scaled_font = (Const.DIALOG_FONT[0], scaled_font_size, Const.DIALOG_FONT[2])
         scaled_wrap_length = int(Const.DIALOG_WRAP_LENGTH * factor)
         self.text_label.config(font=scaled_font, wraplength=scaled_wrap_length)
