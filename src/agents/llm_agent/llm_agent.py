86c96340bace2228b617a7c99f5b0c63f8a2fe2a
inusan-star <haruki.room.14814@icloud.com>
2025-11-01 12:03:44 +0900

    一度ローカル環境で実装して動作確認する

--- src/agents/llm_agent/llm_agent.py
+++ src/agents/llm_agent/llm_agent.py
@@ -1,4 +1,4 @@
-from ygo.util.text import TextUtil
+from src.env.state_data import StateData
 from src.env.action_data import ActionData
 from src.agents.base_agent import BaseAgent
 from src.agents.llm_agent.prompt_generator import PromptGenerator, SYSTEM_PROMPT
@@ -9,16 +9,23 @@ class LLMAgent(BaseAgent):
         super().__init__()
         self.prompt_generator = PromptGenerator()
 
-    def select_action(self, state: dict) -> ActionData:
+    def select_action(self, state: StateData) -> ActionData:
         """状態stateに基づき、行動データActionDataを返す"""
         prompt = self.prompt_generator.generate_instruction_prompt(state=state)
+        commands = state.command_request.commands
 
         messages = [
             {"role": "system", "content": SYSTEM_PROMPT},
             {"role": "user", "content": prompt},
         ]
 
-        pass
+        action = ActionData()
+
+        return action
 
     def update(self, state, action_data, next_state) -> dict | None:
         pass
+
+
+    def _llm_inference(self, messages: list) -> int:
+        
