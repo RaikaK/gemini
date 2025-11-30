f3abd6e995536582abc600d84016602c1a869d1f
inusan-star <haruki.room.14814@icloud.com>
2025-11-30 22:00:12 +0900

    修正

--- src/feature/feature_manager.py
+++ src/feature/feature_manager.py
@@ -48,7 +48,7 @@ class FeatureManager:
         """
         # 初期化
         feature: np.ndarray = np.zeros(
-            (config.TOTAL_CHANNELS, config.HEIGHT, config.WIDTH),
+            (config.TOTAL_CHANNELS_STATE, config.HEIGHT, config.WIDTH),
             dtype=np.float32,
         )
 
@@ -106,7 +106,7 @@ class FeatureManager:
         """
         # 初期化
         feature: np.ndarray = np.zeros(
-            (config.TOTAL_CHANNELS, config.HEIGHT, config.WIDTH),
+            (config.TOTAL_CHANNELS_ACTION, config.HEIGHT, config.WIDTH),
             dtype=np.float32,
         )
 
